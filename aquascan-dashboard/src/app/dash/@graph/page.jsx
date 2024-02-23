67b47cdd67df4955f3013c03144f25a3a820262c
sarve <epiccodewizard@gmail.com>
2024-02-22 16:24:22 -0800

    Lol2

---
+++ aquascan-dashboard/src/app/dash/@graph/page.jsx
@@ -0,0 +1,15 @@
+import {Card, Heading, IconButton} from "@radix-ui/themes";
+
+export default function Page() {
+    return (
+        <Card>
+            <Heading>
+                Graph
+            </Heading>
+            
+            <IconButton>
+                <Cross
+            </IconButton>
+        </Card>
+    );
+}
---
+++ aquascan-dashboard/src/app/dash/layout.jsx
@@ -0,0 +1,9 @@
+export default function Layout({ test1, test2, test3 }) {
+    return (
+        <>
+            {test1}
+            {test2}
+            {test3}
+        </>
+    );
+}
