1f1461b26ade5afdca7b45f9a4b2947a518c236d
sarve <epiccodewizard@gmail.com>
2024-02-22 16:34:57 -0800

    Notifs

--- aquascan-dashboard/src/app/page.tsx
+++ aquascan-dashboard/src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import Image from "next/image";
+import { toast } from "sonner";
+import { Button } from "@/app/ui/button";
 
 export default function Home() {
   return (
-    <>amogus</>
+    <Button
+      variant="outline"
+      onClick={() =>
+        toast("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        })
+      }
+      >
+      Show Toast
+    </Button>
   );
 }
