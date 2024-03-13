4230d58a752cf76b51ae3f915b6f9c1bed0a45d8
mc08 <118325498+splitlane@users.noreply.github.com>
2024-03-12 21:59:17 -0700

    test

--- aquascan-dashboard/src/app/manage/page.jsx
+++ aquascan-dashboard/src/app/manage/page.jsx
@@ -1,7 +1,41 @@
+'use client';
+
+// import dynamic from 'next/dynamic';
+// import { useMemo } from 'react';
+
+// import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet"
+
 export default function Page() {
+  // const Map = useMemo(() => dynamic(
+  //   () => import('@/app/manage/map'),
+  //   { 
+  //     loading: () => <p>A map is loading</p>,
+  //     ssr: false
+  //   }
+  // ), []);
+
+  // return (
+  //   <div>
+  //     <Map position={[51.505, -0.09]} zoom={13} />
+  //   </div>
+  // );
+
+  const position = [51.505, -0.09];
+
   return (
-    <>
-    
-    </>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
